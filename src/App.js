9bd41f254d00417e21c52c3e17be309f76f5d326
yves137 <oneg2981@gmail.com>
2023-10-03 13:11:21 +0200

    update project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Header } from "./Header";
+import { Meme } from "./Meme";
+function App() {
+  return (
+    <div className="">
+      <Header/>
+      <Meme/>
+    </div>
+  );
+}
+
+export default App;
